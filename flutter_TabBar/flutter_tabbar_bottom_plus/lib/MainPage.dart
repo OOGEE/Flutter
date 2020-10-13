bc7433f9c7f8fc7f399e289eec5f2739b2a2695b
unknown <familye@naver.com>
2020-10-13 11:42:48 +0900

    Update flutter_tabbar_bottom_plus

--- flutter_TabBar/flutter_tabbar_bottom_plus/lib/MainPage.dart
+++ flutter_TabBar/flutter_tabbar_bottom_plus/lib/MainPage.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import './Row_Page.dart';
-import './Column_Page.dart';
-import './ListView_Page.dart';
-
 class MainPage extends StatefulWidget {
   @override
   Main createState() => new Main();
@@ -16,7 +12,33 @@ class Main extends State<MainPage> {
   final TextController = TextEditingController();
   String TextString = "";
 
-  List<Widget> _widgets = <Widget> [Row_Page(), Column_Page(), ListView_Page()];
+  List<Widget> _widgets = <Widget>[
+    Row(
+      children: [
+        Expanded(
+          child: RaisedButton(
+            child: Text("버튼 1"),
+            color: Colors.red,
+            onPressed: () {},
+          ),
+        ),
+        Expanded(
+          child: RaisedButton(
+            child: Text("버튼 2"),
+            color: Colors.green,
+            onPressed: () {},
+          ),
+        ),
+        Expanded(
+          child: RaisedButton(
+            child: Text("버튼 3"),
+            color: Colors.blue,
+            onPressed: () {},
+          ),
+        ),
+      ],
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +47,7 @@ class Main extends State<MainPage> {
         title: Text("Bottom TabBar"),
       ),
       body: Center(
-        child: _widgets.elementAt(Index),
+        child: _widgets[0],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
@@ -52,6 +74,111 @@ class Main extends State<MainPage> {
   void _onTap(int index) {
     setState(() {
       Index = index;
+      if (Index == 0) {
+        _widgets = [
+          Row(
+            children: [
+              Expanded(
+                child: RaisedButton(
+                  child: Text("버튼 1"),
+                  color: Colors.red,
+                  onPressed: () {},
+                ),
+              ),
+              Expanded(
+                child: RaisedButton(
+                  child: Text("버튼 2"),
+                  color: Colors.green,
+                  onPressed: () {},
+                ),
+              ),
+              Expanded(
+                child: RaisedButton(
+                  child: Text("버튼 3"),
+                  color: Colors.blue,
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ];
+      }
+      else if (Index == 1) {
+        _widgets = [
+          Column(
+            children: [
+              TextField(
+                controller: TextController,
+              ),
+              RaisedButton(
+                child: Text("버튼"),
+                onPressed: () {
+                  setState(() {
+                    TextString = TextController.text;
+                    _onTap(1);
+                  });
+                },
+              ),
+              Text(TextString),
+            ],
+          ),
+        ];
+      }
+      else if (Index == 2) {
+        _widgets = [
+          ListView(
+            children: [
+              Container(
+                height: 300,
+                child: RaisedButton(
+                  child: Text("버튼 1"),
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                height: 300,
+                child: RaisedButton(
+                  child: Text("버튼 2"),
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                height: 300,
+                child: RaisedButton(
+                  child: Text("버튼 3"),
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                height: 300,
+                child: RaisedButton(
+                  child: Text("버튼 4"),
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                height: 300,
+                child: RaisedButton(
+                  child: Text("버튼 5"),
+                  onPressed: () {},
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                height: 300,
+                child: RaisedButton(
+                  child: Text("버튼 6"),
+                  onPressed: () {},
+                ),
+              ),
+            ],
+          ),
+        ];
+      }
     });
   }
 }
